f65531d6992622436b60771d584fda64f8584499
Muskan Kumari <muskanroy0606@gmail.com>
2026-08-18 19:11:40 +0530

    Update view_users.py

--- N-Queens_Visualizer/N-Queens_Visualizer/view_users.py
+++ N-Queens_Visualizer/N-Queens_Visualizer/view_users.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import sqlite3
-import os
+import os  
 
 # Get path to database relative to this script
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "users.db")
